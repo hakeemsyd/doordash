35d28694a977e1a9d27a262b248b8d9231720d9d
Hakeem Abbas <hakeemsyd@gmail.com>
2017-01-21 21:15:22 -0800

    added thumbnail with restaurant name

--- app/src/main/java/sample/doordash/com/doordash/ui/RestaurantViewHolder.java
+++ app/src/main/java/sample/doordash/com/doordash/ui/RestaurantViewHolder.java
@@ -2,8 +2,11 @@ package sample.doordash.com.doordash.ui;
 
 import android.content.Context;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.ToggleButton;
+
+import com.squareup.picasso.Picasso;
 
 import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
@@ -23,14 +26,19 @@ public class RestaurantViewHolder {
     private View mView;
     private ToggleButton mFav;
     private TextView mName;
+    private ImageView mThumbnail;
     private Storage mStorage;
+    private Context mContext;
 
     public RestaurantViewHolder(Context context, View view, Restaurant restaurant){
         mStorage = new Storage(context);
+        mContext = context;
         mView = view;
         mRestaurant = restaurant;
         mName = (TextView) mView.findViewById(R.id.name);
         mFav = (ToggleButton) mView.findViewById(R.id.favorite);
+        mThumbnail = (ImageView) mView.findViewById(R.id.rest_thumbnail);
+
         update();
         mFav.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,6 +64,7 @@ public class RestaurantViewHolder {
     private void update(){
         mName.setText(mRestaurant.mName);
         mFav.setChecked(false);
+        Picasso.with(mContext).load(mRestaurant.mCoverImageSrc).into(mThumbnail);
         mStorage.isFavourite(mRestaurant)
                 .subscribeOn(Schedulers.computation())
                 .observeOn(AndroidSchedulers.mainThread())
